08fbdca7431d33d621d9f0267d230c0a33e336b4
TEDU <83123095@qq.com>
2021-07-08 10:23:44 +0800

    第二天

---
+++ V2/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,44 @@
+package com.webserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+/**
+ * 处理与指定客户端的HTTP交互
+ * HTTP协议要求客户端与服务端交互为"一问一答"原则.因此当客户端连接
+ * 服务端后,服务端便会启动一个线程来执行ClientHandler.
+ * 这里分为三步完成一次HTTP交互处理:
+ * 1:解析请求(读取客户端发送过来的HTTP请求内容)
+ * 2:处理请求(分析请求内容理解意图并进行对应的处理)
+ * 3:发送响应(将处理结果包含在一个HTTP响应内容中,将其回复给客户端)
+ */
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+    public void run() {
+        try{
+            //1解析请求
+            InputStream in = socket.getInputStream();
+            int d;
+            while((d = in.read())!=-1){
+                System.out.print((char)d);
+            }
+            //2处理请求
+
+            //3发送响应
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                //交互完毕后与客户端断开连接(HTTP协议要求)
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
