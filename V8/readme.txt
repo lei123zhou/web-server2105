a867559cd327b9e8c429bfbfbd15b73325e1faaa
TEDU <83123095@qq.com>
2021-07-12 11:38:07 +0800

    第四天

--- V8/readme.txt
+++ V8/readme.txt
@@ -8,3 +8,11 @@
 2:在响应对象中定义一个响应中应当包含的三部分信息(状态行,响应头,响应正文)
   对应的属性
 3:定义方法flush,用于发送响应内容
+
+第二步:拆分处理请求的工作
+实现:
+1:新建包:com.webserver.servlet
+2:在servlet包中新建类:DispatcherServlet,并定义service方法处理
+  请求的工作
+3:将ClientHandler中处理请求的工作拆分到DispatcherServlet的service
+  方法中.
--- V8/src/main/java/com/webserver/core/ClientHandler.java
+++ V8/src/main/java/com/webserver/core/ClientHandler.java
@@ -3,6 +3,7 @@ package com.webserver.core;
 
 import com.webserver.http.HttpRequest;
 import com.webserver.http.HttpResponse;
+import com.webserver.servlet.DispatcherServlet;
 
 import java.io.*;
 import java.net.Socket;
@@ -31,20 +32,9 @@ public class ClientHandler implements Runnable{
             HttpResponse response = new HttpResponse(socket);
 
             //2处理请求
-            String path = request.getUri();
-            File file = new File("./webapps"+path);
+            DispatcherServlet servlet = new DispatcherServlet();
+            servlet.service(request,response);
 
-            if(file.exists()&&file.isFile()){
-                //文件存在则直接响应
-                response.setEntity(file);
-            }else{
-                //文件不存在或者定位的是一个目录,则响应404
-                response.setStatusCode(404);
-                response.setStatusReason("NotFound");
-                response.setEntity(
-                    new File("./webapps/root/404.html")
-                );
-            }
             //3发送响应
             response.flush();
 
