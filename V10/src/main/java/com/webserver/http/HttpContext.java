96640127320ab1059b3b082aac39e1394058ca79
TEDU <83123095@qq.com>
2021-07-13 17:12:27 +0800

    第5天

--- V10/src/main/java/com/webserver/http/HttpContext.java
+++ V10/src/main/java/com/webserver/http/HttpContext.java
@@ -1,6 +1,12 @@
 package com.webserver.http;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.xml.sax.SAXException;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,12 +36,37 @@ public class HttpContext {
      * 初始化mimeMapping
      */
     private static void initMimeMapping(){
-        mimeMapping.put("html","text/html");
-        mimeMapping.put("css","text/css");
-        mimeMapping.put("js","application/javascript");
-        mimeMapping.put("gif","image/gif");
-        mimeMapping.put("png","image/png");
-        mimeMapping.put("jpg","image/jpeg");
+//        mimeMapping.put("html","text/html");
+//        mimeMapping.put("css","text/css");
+//        mimeMapping.put("js","application/javascript");
+//        mimeMapping.put("gif","image/gif");
+//        mimeMapping.put("png","image/png");
+//        mimeMapping.put("jpg","image/jpeg");
+        /*
+            web.xml是tomcat提供的文件,里面保存了所有资源后缀与对应
+            的Content-Type头的值.
+            通过解析config/web.xml文件初始化mimeMapping
+            将根元素中所有名为<mime-mapping>的子标签获取到,并将其中
+            的子标签:
+            <extension>中的文本作为key
+            <mime-type>中的文本作为value
+            以一组键值对形式存入mimeMapping这个Map.
+            最终完成后,mimeMapping应该有1011个元素.
+         */
+        try {
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read("./config/web.xml");
+            Element root = doc.getRootElement();
+            List<Element> list = root.elements("mime-mapping");
+            for(Element e : list){
+                String key = e.elementText("extension");
+                String value = e.elementText("mime-type");
+                mimeMapping.put(key,value);
+            }
+            System.out.println(mimeMapping.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -45,5 +76,9 @@ public class HttpContext {
      */
     public static String getMimeType(String ext){
         return mimeMapping.get(ext);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMimeType("gif"));
     }
 }
