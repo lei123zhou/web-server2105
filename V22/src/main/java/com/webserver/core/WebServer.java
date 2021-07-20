c8aa80ce4d27917626f50371ce4408a7ae543f59
TEDU <83123095@qq.com>
2021-07-20 17:53:35 +0800

    第9天

--- V22/src/main/java/com/webserver/core/WebServer.java
+++ V22/src/main/java/com/webserver/core/WebServer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer主类
@@ -11,11 +13,13 @@ import java.net.Socket;
  */
 public class WebServer {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;
 
     public WebServer(){
         try {
             System.out.println("正在启动服务端...");
             serverSocket = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,11 +32,9 @@ public class WebServer {
                 System.out.println("等待客户端链接...");
                 Socket socket = serverSocket.accept();
                 System.out.println("一个客户端链接了!");
-
                 //启动一个线程来处理该客户端的交互
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+                threadPool.execute(handler);
             }
         } catch (IOException e) {
             e.printStackTrace();
